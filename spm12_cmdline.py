5f354ecf30c9917c33a7244d65fbe67d9d7317ea
Crispy13 <47196430+Crispy13@users.noreply.github.com>
2021-08-05 11:10:12 +0900

    Update spm12_cmdline.py

--- spm12_cmdline.py
+++ spm12_cmdline.py
@@ -3,4 +3,4 @@ import sys
 if sys.version_info[0] < 3:
     raise Exception("Python 3.0+ is needed.")
     
-subprocess.run(["python", "/data/eck/CustomScriptsArchive/spm12_scripts/spm12_cmdline_main.py"] + sys.argv[1:])
+subprocess.run(["python", "./spm12_scripts/spm12_cmdline_main.py"] + sys.argv[1:])
